7c9bfed3c6ef15c66453878fdd8c96b0052fcc39
Marishik12 <koretskamaryna10@gmail.com>
2023-09-21 19:52:34 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+# import requests
+# def first_function():
+#     pass
+# class Human:
+#     pass
+# rq = requests
+# first_f = first_function
+# nick = Human
+# print(requests.__name__)
+# print(rq.__name__)
+# print(first_function.__name__)
+# print(first_f.__name__)
+# print(Human.__name__)
+# print(nick.__name__)
+
+# data = "string"
+# print(hasattr(data, "reverse"))
+# print(hasattr(data, "index"))
+# data = "string"
+# print(getattr(data, "startswith"))
+# print(getattr(data, "startswith", None))
+# print(getattr(data, "reverse", None))
+# data = "string"
+# def first_function():
+#     pass
+# print(callable(data))
+# print(callable(first_function))
+
+# class First_class:
+#     pass
+# class Second_class(First_class):
+#     pass
+# print(issubclass(First_class,Second_class))
+# print(issubclass(Second_class,First_class))
+# class First_class:
+#     pass
+# class Second_class(First_class):
+#     pass
+# obj_from_class_2 = Second_class()
+# print(isinstance(obj_from_class_2, First_class))
+# print(isinstance(obj_from_class_2, Second_class))
+
+# import inspect
+# import requests
+# print(inspect.ismodule(requests))
+# print(inspect.isclass(requests))
+# print(inspect.isfunction(requests))
+
+# import inspect
+# import requests
+# print(inspect.getmodule(requests.get))
+# print(inspect.getmodule(list))
+
+# import sys
+# print(sys.executable)
+# print(sys.version)
+#
+# import sys
+# import requests
+# for module_name, module_path in sys.modules.items():
+#     print(module_name, module_path)
